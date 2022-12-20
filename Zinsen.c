c1ebc166999d4e0c9e552a82c8ffab81c2a1dc02
TimSpakowski <119943071+TimSpakowski@users.noreply.github.com>
2022-12-20 13:17:55 +0100

    Create Zinsen.c

---
+++ Zinsen.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main () {
+ 
+  int Jahre;
+  
+  printf("Geben sie die Anzahl der Jahre ein: ");
+  scanf("%d", &Jahre);
+  
+  int i = 0;
+  float Zins = 0.045
+    
+  float Betrag;
+  
+  printf("Geben sie ihren Betrag ein: ");
+  scanf("%f", &Betrag);
+  
+  float n;
+  n = Betrag * Zins;
+  
+  int Jahr = 1;
+  
+    if (Betrag >= 0 && Jahre >= 0) { 
+  
+      for(i=0;i<=Jahre;i++){
+   
+        Betrag = Betrag + n;
+    
+        printf("Der Betrag beträgt in Jahr %d %f €\n", Jahr, Betrag);
+        Jahr++
+    
+      }
+    }else{
+      printf("Ungültige Eingabe");
+  }
+  
+}
